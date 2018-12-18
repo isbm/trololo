a518e6f6ba7d88bbe23a9268450b5d047cdf1686
Bo Maryniuk <bo@suse.de>
2018-12-18 22:07:36 +0100

    Add UT for yaml parser

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,7 +4,8 @@ Unit tests for App
 """
 
 import pytest
-from unittest.mock import MagicMock, patch
+import yaml.parser
+from unittest.mock import MagicMock, patch, mock_open
 
 from trololo.app import TrololoApp
 import trololo.exceptions
@@ -27,7 +28,7 @@ class TestTrololoApp(object):
     Test application functionality unit tests.
     """
     @patch("os.path.exists", MagicMock(return_value=False))
-    def test_runner_raises_on_config_absent(self, app):
+    def test_exception_on_config_notfound(self, app):
         """
         Test if runner raises an exception, once configuration is not found.
 
@@ -37,3 +38,15 @@ class TestTrololoApp(object):
             app.run()
 
         assert "Configuration file 'edward.conf' is not found in the current directory." in str(ex)
+
+    def test_exception_on_config_failure(self, app):
+        """
+        Test if runner raises an exception, once configuration is wrong.
+
+        :param app:
+        :return:
+        """
+        with patch("trololo.app.open", mock_open(read_data="non-redundant\n: fan failure"), create=True):
+            with pytest.raises(yaml.parser.ParserError) as ex:
+                app.run()
+            assert "expected '<document start>', but found '<block mapping start>'" in str(ex)
