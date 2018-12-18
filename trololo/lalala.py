f3bb08e09e725ff7df86b8a3a1e583cc40bdfd5f
Bo Maryniuk <bo@suse.de>
2018-12-18 19:51:27 +0100

    Add board implementation

--- trololo/lalala.py
+++ trololo/lalala.py
@@ -20,3 +20,39 @@ class TrololoObject(object):
         """
         raise NotImplementedError("Loading not implemented")
 
+
+class TrololoBoard(TrololoObject):
+    """
+    Trello Board.
+    """
+    def __init__(self, client, **kwargs):
+        """
+        :param client:
+        :param id:
+        :param name:
+        """
+        self.__client = client
+        self.id = kwargs.get("id")
+        self.name = kwargs.get("name")
+        self.descr = kwargs.get("desc")
+        self.link = kwargs.get("shortUrl")
+        self.url = kwargs.get("url")
+        self.last_viewed = kwargs.get("dateLastView")
+
+    def list_cards(self):
+        """
+        List all cards.
+
+        :return:
+        """
+
+    @staticmethod
+    def load(client, data):
+        """
+        Create board object instance from JSON.
+
+        :param client: Client instance
+        :param data: JSON data
+        :return: TrololoBoard object
+        """
+        return TrololoBoard(client=client, **data)
